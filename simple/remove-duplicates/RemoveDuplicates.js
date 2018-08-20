3cd0e0d02864c35c5d79a14b8c93e4a9fbb429f4
karis <845201776@qq.com>
2018-08-20 22:34:30 +0800

    添加数组去重

---
+++ simple/remove-duplicates/RemoveDuplicates.js
@@ -0,0 +1,23 @@
+/**
+ *  Remove Duplicates from Sorted Array
+ * 
+ * Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * 
+ * example
+ * Given nums = [1,1,2],
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+ * It doesn't matter what you leave beyond the returned length.
+
+ */
+var removeDuplicates = function(nums) {
+    nums.forEach((n, index) => {
+        console.log(n);
+        console.log(index);
+             while(n == nums[index +1]){
+                 nums.splice(index +1, 1);
+             }
+      });
+     
+     return nums.length;
+ };
